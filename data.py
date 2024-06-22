bc9ecf9d0a5dfab7162a52cf586e8749a632c68f
Kramarenkoo <leshakram1@yandex.ru>
2024-06-22 14:09:54 +0300

    upadate

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSkccFemKSPiThT66-1OFFpuYC7xjfmluk4jKHM3axi2B4fzN2k0PG-DsPx0zrri6jCexpcxBZG4lv0/pub?gid=729112787&single=true&output=csv',  sep=',')
+#df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSkccFemKSPiThT66-1OFFpuYC7xjfmluk4jKHM3axi2B4fzN2k0PG-DsPx0zrri6jCexpcxBZG4lv0/pub?gid=729112787&single=true&output=csv',  sep=',')
+df = pd.read_csv('smartphones.csv', sep=',')
 all_brand = df['Brand'].unique()
 
 
